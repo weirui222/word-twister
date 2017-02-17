4edffe2bd070c3d133114df9a3d38d15beb2bed4
Donnie Ellstrom <dsvene@gmail.com>
2017-02-17 10:01:51 -0800

    Add skip functionality

--- word-scrambler/src/Player.jsx
+++ word-scrambler/src/Player.jsx
@@ -2,12 +2,7 @@ import React, { Component } from 'react';
 import './Player.css';
 import Modal from 'react-modal';
 
-const wordbank =["thundering", "javascript", "immutable", "variable", "vulgar", "serialize", "vagabond", "imminent", "shish kabob", "scold", "deserve", "apologise", "person", "elitist", "measure", "parallel", "examine", "mindful", "electric", "nefarious", "bitter", "treasure", "elongate", "judgement", "lacking", "excessive", "satisfy", "apartment", "superfluous", "steel", "chickens", "fairies", "delight", "education", "berserker", "triangulate", "crooked","abandoned", "profit", "erratic", "semantics", "maniacal", "breathe", "sticker", "unused", "grotesque", "challenger", "mysterious", "suffer", "semi-permeable", "horrible", "outrageous", "arithmetic", "derivative", "rhythm", "trivial", "succulent", "tranquil", "hallmark", "humongous", "concentrate", 'website', 'virtual', 'version', 'utility', 'toolbar',
-'storage', 'spyware', 'spammer', 'scanner', 'runtime', 'restore',
-'program', 'process', 'privacy', 'printer', 'podcast', 'offline',
-'network', 'monitor', 'malware', 'lurking', 'keyword', 'integer',
-'exabyte', 'encrypt', 'dynamic', 'digital', 'desktop', 'compile',
-'command', 'captcha', 'browser'];
+const wordbank =["thundering", "javascript", "immutable", "variable", "vulgar", "serialize", "vagabond", "imminent", "teriyaki", "scalding", "deserve", "apologize", "person", "elitist", "measure", "parallel", "examine", "mindful", "electric", "nefarious", "bitter", "treasure", "elongate", "judgement", "lacking", "excessive", "satisfy", "apartment", "superfluous", "steel", "chickens", "fairies", "delight", "education", "berserker", "triangulate", "crooked","abandoned", "profit", "erratic", "semantics", "maniacal", "breathe", "sticker", "unused", "grotesque", "challenger", "mysterious", "suffer", "semi-permeable", "horrible", "outrageous", "arithmetic", "derivative", "rhythm", "trivial", "succulent", "tranquil", "hallmark", "humongous", "concentrate", 'website', 'virtual', 'version', 'utility', 'toolbar', 'storage', 'spyware', 'spammer', 'scanner', 'runtime', 'restore', 'program', 'process', 'privacy', 'printer', 'podcast', 'offline', 'network', 'monitor', 'malware', 'lurking', 'keyword', 'integer', 'exabyte', 'encrypt', 'dynamic', 'digital', 'desktop', 'compile', 'command', 'captcha', 'browser'];
 
 
 const customStyles = {
@@ -140,14 +135,14 @@ class Player extends Component {
   render() {
   	let listCorrect = [];
   	if (this.state.correctWords.length > 0) {
-  		listCorrect = this.state.correctWords.map((correctWord, index) => { 
+  		listCorrect = this.state.correctWords.map((correctWord, index) => {
   		return <li key={index}> {correctWord} </li> });
   	}
 
 
     return (
       <div className="playerContainer">
-      	<Modal 
+      	<Modal
            isOpen={this.state.gameOver}
            style={customStyles}
            contentLabel="Example Modal">
@@ -167,23 +162,24 @@ class Player extends Component {
             <div>
               <h1><span className="playerPrompt">Untwist: </span><span className="scrambledWord">{this.state.word}</span></h1>
               <button className="btn btn-default hintBtn" onClick={(e) => this.hintWord()}>Hint!</button>
+              <button className="btn btn-default skipBtn" onClick={(e) => this.compareWord(e)}>Skip</button>
             </div>
             <form className="submitForm" onSubmit={(e) => this.compareWord(e)}>
-              <input placeholder="Enter the correct word" className="inputField" type="text"
+              <input placeholder="Enter the correct word" className="inputField" type="text" required
               			 onChange={e => this.inputChange(e)}
                      value={this.state.playerInput}
                   />
                   <hr className="inputHR" />
                   <button type="submit" className="btn btn-default submitBtn">Submit</button>
-                  
+
             </form>
         </div>
         <div className="col-xs-2 col-xs-offset-(-2)">
           <div className="playerScoreBox">
-          	
+
           	<p className="scoreTitle">Correct Words:</p>
           	<div>
-	          	<ul> 
+	          	<ul>
 	            	{listCorrect}
 	            </ul>
           	</div>
