09ece5bf2c2a17903db3797ad04681a0032181fa
Donnie Ellstrom <dsvene@gmail.com>
2017-03-03 15:23:35 -0800

    Fix styling issues

--- src/App.js
+++ src/App.js
@@ -11,9 +11,6 @@ class App extends Component {
         <div>
         	<header></header>
           <Player />
-          <footer>
-          	<p ><a className='info' href='https://github.com/Crateros/donnie-congcong-wordgame'>Created by CongCong Li &#38; Donnie Ellstrom</a></p>
-          </footer>
         </div>
       </div>
     );
